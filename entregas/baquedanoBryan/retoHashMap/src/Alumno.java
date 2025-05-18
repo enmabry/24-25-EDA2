7186942540d11117743dedd6ff00af6610a5a37c
enmabry <bryanbaquedano11@gmail.com>
2025-05-18 19:37:12 +0200

    Se han creado los diagramas en los cuales me basaré para hacer el programa de el hash

---
+++ entregas/baquedanoBryan/retoHashMap/src/Alumno.java
@@ -0,0 +1,12 @@
+public class Alumno {
+    protected String nombre;
+    protected String apellido;
+    protected int nota;
+
+    public Alumno(String nombre, String apellido, int nota) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.nota = nota;
+    }
+    
+}
