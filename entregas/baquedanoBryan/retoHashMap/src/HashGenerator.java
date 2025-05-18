1d3b668a31dd141d296f07c6eda6b3de0e12309e
enmabry <bryanbaquedano11@gmail.com>
2025-05-18 23:28:03 +0200

    Clase HashGenerator
    esta es una clase "tonta" en el sentido en el que no tiene atributos ni contructor, porque no los necesita, ya que solo recibe el string y hashea

---
+++ entregas/baquedanoBryan/retoHashMap/src/HashGenerator.java
@@ -0,0 +1,19 @@
+public class HashGenerator {
+
+    
+    public static int generarHashSimple(String datos) {
+        int hash = 0;
+        for (int i = 0; i < datos.length(); i++) {
+            char caracter = datos.charAt(i);
+            int ascii = (int) caracter;
+            hash += ascii * (i + 1); 
+        }
+        hash = hash % 1000000; 
+        return hash;
+    }
+
+    
+    public static String convertirAHexadecimal(int hash) {
+        return Integer.toHexString(hash);
+    }
+}
